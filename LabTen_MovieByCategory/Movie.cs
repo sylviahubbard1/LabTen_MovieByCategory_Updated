d8c53570e7d9d28f2472d36a49e2c33c439afb31
sylviahubbard1 <sylviahubbard@gmail.com>
2016-10-19 11:17:55 -0400

    Movies by Category

---
+++ LabTen_MovieByCategory/Movie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabTen_MovieByCategory
+{
+    public class Movie
+    {
+        public string title = "";
+        //public Category category;
+        public string category = "";
+        public Movie(string Title, string movieCategory)
+        {
+            this.title = Title;
+            this.category = movieCategory;
+            //this.category = movieCategoryConverter(movieCategory);
+
+
+        }
+        public enum Category { ANIMATED=4 ,DRAMA=3, HORROR=18, SCIFI, COMEDY, UNCATEGORIZED};
+
+        
+        private Category movieCategoryConverter(string category)
+        {
+           
+
+            if (category == "animated" )
+            {
+                return Category.ANIMATED;
+
+            }
+            else if (category == "drama")
+            {
+                return Category.DRAMA;
+            }
+            else if (category == "horror")
+            {
+                return Category.HORROR;
+            }
+            else if (category == "scifi")
+            {
+                return Category.SCIFI;
+            }
+            else if (category == "comedy") 
+            {
+                return Category.COMEDY;
+            }
+            else
+            {
+                return Category.UNCATEGORIZED;
+                
+            }
+            
+        }
+    }
+}
+    
--- LabTen_MovieByCategory/Program.cs
+++ LabTen_MovieByCategory/Program.cs
@@ -10,6 +10,47 @@ namespace LabTen_MovieByCategory
     {
         static void Main(string[] args)
         {
+            //Movie.Category myAnimated = Movie.Category.HORROR;
+
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+            Console.WriteLine("There are " + MovieIO.MovieCatalog.Count + " movies in this list.");
+
+            string movieCategorySelect = "";
+            string Cont = "";
+
+            //List<Movie.Category> listOfCategories = new List<Movie.Category>();
+
+            //listOfCategories.AddRange(Enum.GetValues(typeof(Movie.Category)).Cast<Movie.Category>());
+
+            do
+            {
+                ////foreach (var genRe in listOfCategories)                                                                                                                                                                                                                                                                                                                                            
+                //{
+                //    Console.WriteLine( (int)genRe+"-"+  genRe.ToString()+" ");
+                //}
+                Console.WriteLine("What Category are you interested in? 1. drama  2. scifi 3. musical 4. animated 5.comedy");
+                movieCategorySelect = Console.ReadLine();
+
+                int countMovieMatch = 0;
+
+                foreach (Movie l1 in MovieIO.MovieCatalog.OrderBy(m => m.title))
+                {
+
+                    if (movieCategorySelect == l1.category)
+                    {
+                        Console.WriteLine("Title: " + l1.title);
+                        countMovieMatch++;
+
+                    }
+                    if (countMovieMatch == 10)
+                        break;
+                }
+                Console.WriteLine("Do you want to continue? Type yes or no");
+                Cont = Console.ReadLine().ToLower().Trim();
+            } while (Cont == "yes"); Console.WriteLine("Thank you for your inquiry");
         }
+            
     }
 }
+
